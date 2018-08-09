b11005a598d76ea25799ac79bffc1807c8e76a0b
lzepeda <lzepeda@math.mit.edu>
2018-08-09 13:51:31 -0700

    modified the functions to match relative error

--- NLSE/CheckRelError.py
+++ NLSE/CheckRelError.py
@@ -1,10 +1,20 @@
-from keras.callbacks import Callback 
+from keras.callbacks import Callback
 import numpy as np
 import timeit
 
+
+
+def rel_error(model, X, Y, meanY =0):
+    Yhat = model.predict(X)
+    dY = Yhat - Y
+    axis = tuple(range(np.size(dY.shape))[1:])
+    return  np.sqrt(np.sum(dY**2, axis = axis)/
+                     np.sum((Y+ meanY)**2, axis = axis))
+
+
 class CheckRelError(Callback):
     def __init__(self, X_train, Y_train, X_test, Y_test,
-                verbose = False, period=1):
+                verbose = False, period=1, errorFun = rel_error):
         super(CheckRelError, self).__init__()
         self.X_train = X_train
         self.Y_train = Y_train
@@ -18,6 +28,7 @@ class CheckRelError(Callback):
         self.best_err_test_max  = 100
         self.best_err_train_ave = 100
         self.best_err_test_ave  = 100
+        self.errorFun  = errorFun
 
     def on_train_begin(self, logs=None):
         self.best_err_train     = 100
@@ -28,12 +39,12 @@ class CheckRelError(Callback):
         self.best_err_test_ave  = 100
 
     def on_epoch_end(self, epoch, logs=None):
-        
+
         if((epoch)%self.period == 0):
             t1 = timeit.default_timer()
 
-            err_train = rel_error(self.model, self.X_train, self.Y_train)
-            err_test  = rel_error(self.model, self.X_test, self.Y_test)
+            err_train = self.errorFun(self.model, self.X_train, self.Y_train)
+            err_test  = self.errorFun(self.model, self.X_test, self.Y_test)
 
             betr = np.mean(err_train)
             bete = np.mean(err_test)
@@ -47,7 +58,7 @@ class CheckRelError(Callback):
 
             bestAv = (self.best_err_train_ave+self.best_err_test_ave+
                 (self.best_err_train_max+self.best_err_test_max)/5)
-            tempAv = (betr+bete+(betrm+betem)/5) 
+            tempAv = (betr+bete+(betrm+betem)/5)
 
             if ( bestAv > tempAv ):
                 self.best_err_train_ave = betr
@@ -58,24 +69,21 @@ class CheckRelError(Callback):
             t2 = timeit.default_timer()
             if(self.verbose):
                 print("runtime of checkresult = %.2f secs" % (t2-t1))
-                print('best train and test error = %.1e, %.1e' % 
+                print('best train and test error = %.1e, %.1e' %
                         (self.best_err_train, self.best_err_test))
-                print('best train and test error ave/max = %.1e, %.1e, %.1e, %.1e' % 
-                        (self.best_err_train_ave, self.best_err_train_max, 
+                print('best train and test error ave/max = %.1e, %.1e, %.1e, %.1e' %
+                        (self.best_err_train_ave, self.best_err_train_max,
                          self.best_err_test_ave, self.best_err_test_max))
 
         def on_train_end(self, logs=None):
             print('best train and test error = %.1e, %.1e' % (self.best_err_train, self.best_err_test))
             print('best train and test error ave/max = %.1e, %.1e, %.1e, %.1e' % (self.best_err_train_ave, self.best_err_train_max, self.best_err_test_ave, self.best_err_test_max))
 
-def rel_error(model, X, Y):
-    Yhat = model.predict(X)
-    dY = Yhat - Y  
-    axis = tuple(range(np.size(dY.shape))[1:])
-    return  np.sqrt(np.sum(dY**2, axis = axis)/
-                     np.sum(Y**2, axis = axis))
 
-#We may want to save th emodel with the lowest relative error 
+
+
+
+#We may want to save th emodel with the lowest relative error
 # class ModelRelErrCheckpoint(Callback):
 
 #     def __init__(self, filepath, verbose=False,
--- NLSE/utils.py
+++ NLSE/utils.py
@@ -1,9 +1,9 @@
 """
-  File with the utility functions 
+  File with the utility functions
   Y Fan, L Lin, L Ying, L Zepeda-Nunez, A multiscale neural network based on hierarchical matrices,
   arXiv preprint arXiv:1807.01883
 
-  
+
 """
 import numpy as np
 from keras import backend as K
@@ -16,8 +16,9 @@ def padding(x, size):
 def rel_err_loss(y_true, y_pred):
     return K.sum(K.square(y_pred - y_true), axis=-1)/K.sum(K.square(y_true), axis=-1)
 
-def initParser():
-	parser = argparse.ArgumentParser(description='NLSE - MNN-H')
+def initParser(descStr,
+			   trainResStr='trainresultH.txt'):
+	parser = argparse.ArgumentParser(description=descStr)
 	parser.add_argument('--epoch', type=int, default=4000, metavar='N',
 	                    help='input number of epochs for training (default: %(default)s)')
 	parser.add_argument('--input-prefix', type=str, default='nlse2v2', metavar='N',
@@ -38,39 +39,27 @@ def initParser():
 	                    help='suffix output filename(default: )')
 	parser.add_argument('--percent', type=float, default=0.5, metavar='precent',
 	                    help='percentage of number of total data(default: %(default)s)')
-	parser.add_argument('--sum-file', type=str, default='trainresultH.txt', metavar='N',
+	parser.add_argument('--sum-file', type=str, default=trainResStr, metavar='N',
 	                    help='file to summarize the error of the training (default: %(default)s)')
 	return parser
 
-def preTreatDataNSLE(InputArray, OutputArray,n_train,n_test,output):
-	"""preTreatData(InputArray, OutputArray,n_train,n_test)
-	function to pre-treat the data for the NSLE
-	"""
-	# obtaining the number of Samples 
+def splitData(InputArray, OutputArray,n_train,n_test,output):
+	"""function for splitting the data"""
 	Nsamples = InputArray.shape[0]
-	# computing mean of the input and output data
-	mean_out = np.mean(OutputArray[0:n_train, :])
-	mean_in  = np.mean(InputArray[0:n_train, :])
 
-	output("mean of input / output is %.6f\t %.6f" % (mean_in, mean_out))
-	
-	# treating the data
-	InputArray /= mean_in * 2
-	InputArray -= 0.5
-	OutputArray -= mean_out
-	
-	# splitting the data 
-	X_train = InputArray[0:n_train, :] 
+	X_train = InputArray[0:n_train, :]
 	Y_train = OutputArray[0:n_train, :]
 	X_test  = InputArray[(Nsamples-n_test):Nsamples, :]
 	Y_test  = OutputArray[(Nsamples-n_test):Nsamples, :]
+
+
 	n_input = X_train.shape[1]
 	n_output= Y_train.shape[1]
-	
+
 	output("[n_input, n_output] = [%d, %d]" % (n_input, n_output))
 	output("[n_train, n_test] = [%d, %d]" % (n_train, n_test))
 
-	# reshaping hte input data 
+	# reshaping hte input data
 	X_train = np.reshape(X_train, [X_train.shape[0], X_train.shape[1], 1])
 	X_test  = np.reshape(X_test,  [X_test.shape[0],  X_test.shape[1],  1])
 
